134ec61cac8ae71892bce1aab40c259dd97b7a4e
ImadABID <imad.abied@gmail.com>
2022-11-09 17:42:47 +0100

    adding a tracking closure

--- Client/rule_transformer/target_rule.js
+++ Client/rule_transformer/target_rule.js
@@ -1,15 +1,24 @@
-let tracked_params = [];
+let tracked_params = {};
 
-tracked_params.push(
-    {
-        type : 'trigger', // trigger or action
-        var_name : 'Office365Mail.newEmail.Subject',
-        value    : '[IFTTT] Urgent mail',
-        accessed : false,
-
-        // get value ()=>{return this.value},
+let access_tracker = (varValue)=>{
+    let _type = 'trigger' // trigger or action
+    let _value    = varValue
+    let _accessed = false
+    return {
+        get value(){_accessed = true; return _value;},
+        get accessed(){return _accessed;}
     }
-)
+}
+
+let Office365Mail = require('./trigger_action_modules').Office365Mail;
+let Slack = require('./trigger_action_modules').Slack;
+
+tracked_params['Office365Mail.newEmail.Subject'] = access_tracker(Office365Mail.newEmail.Subject);
+
+console.log(tracked_params['Office365Mail.newEmail.Subject'].accessed)
+console.log(tracked_params['Office365Mail.newEmail.Subject'].value)
+console.log(tracked_params['Office365Mail.newEmail.Subject'].accessed)
+
 
 // Rule
 // let Office365Mail = require('./trigger_action_modules').Office365Mail;
