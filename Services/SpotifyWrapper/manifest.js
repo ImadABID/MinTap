ab0d613cf78a8e50240dd1ea8a87567b58f0a770
ImadABID <imad.abied@gmail.com>
2023-01-18 17:22:59 +0100

    adding SpotifyWrapper manifest

---
+++ Services/SpotifyWrapper/manifest.js
@@ -0,0 +1,75 @@
+//MANIFEST.js
+
+// Only for trigger manifest
+let dataArray = {
+  "Spotify.currentTrack.artistNames" : null,
+  "Spotify.currentTrack.trackName" : null,
+  "Spotify.currentTrack.albumName" : null,
+  "Spotify.currentTrack.releaseDate" : null,
+  "Spotify.currentTrack.trackDuration" : null,
+}
+
+class SpotifyClass { 
+
+  constructor(){
+
+    // ------ Define the service object --------
+    
+    this.currentTrack = {
+      get artistNames(){
+        return dataArray["Spotify.currentTrack.artistNames"];
+      },
+      get trackDuration(){
+        return dataArray["Spotify.currentTrack.trackDuration"];
+      },
+      get albumName(){
+        return dataArray["Spotify.currentTrack.albumName"];
+      },
+      get Duration(){
+        return dataArray["Spotify.currentTrack.releaseDate"];
+      },
+    };
+
+    // -----------------------------------------
+
+  }
+
+  static async SpotifyClassFactory(
+    askedFields,
+    properties = null
+  ){
+
+    // Only for trigger manifest
+    // ------ Type here the code that populates dataArray --------
+
+    const API_URL = "127.0.0.1:3006";
+
+    fetch(`http://${API_URL}/tap`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        askedFields: askedFields,
+        properties: properties
+      }),
+    })
+    .then(res => res.json())
+    .then((json)=>{
+      dataArray = json;
+      resolve(new SpotifyClass());
+    })
+    .catch(err => console.error(err));
+
+    // -----------------------------------------------------------
+
+  }
+
+}
+
+// Testing
+
+// const main = async ()=>{
+//   const Spotify = await SpotifyClass.SpotifyClassFactory([]);
+//   console.log(Spotify.currentTrack.title);
+// }
+
+// main();
